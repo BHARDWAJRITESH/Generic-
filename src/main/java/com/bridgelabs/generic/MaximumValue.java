bd7cb7711123b2806a826de36625b6481a1a320f
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-10-11 22:23:50 +0530

    Add: Write and testing  refractor1 for one generic method to find the maximum

--- src/main/java/com/bridgelabs/generic/MaximumValue.java
+++ src/main/java/com/bridgelabs/generic/MaximumValue.java
@@ -1,59 +1,15 @@
 package com.bridgelabs.generic;
 
 public class  MaximumValue {
-	/**
-	 * Creating method findMaximum
-	 * @param firstValue
-	 * @param secondValue
-	 * @param thirdValue
-	 * @return
-	 */
-	public Integer findMaximum(Integer firstValue, Integer secondValue, Integer thirdValue) {
-		Integer maximum = firstValue;
-		if(secondValue.compareTo(maximum)> 0) {
-			maximum = secondValue;
-		}
-		if(thirdValue.compareTo(maximum)> 0) {
-			maximum = thirdValue;
-		}
-		System.out.println("Maximum Integer number is " + maximum);
-		return maximum;		
+	public <T extends Comparable<T>> T findMaximum(T firstValue, T secondValue, T thirdValue) {
+	T maximum = firstValue;
+	if(secondValue.compareTo(maximum)> 0) {
+		maximum = secondValue;
 	}
-	/**
-	 * Creating method findMaximum which return Float value
-	 * @param firstValue
-	 * @param secondValue
-	 * @param thirdValue
-	 * @return
-	 */
-	public Float findMaximum(Float firstValue, Float secondValue, Float thirdValue) {
-		Float maximum = firstValue;
-		if(secondValue.compareTo(maximum)> 0) {
-			maximum = secondValue;
-		}
-		if(thirdValue.compareTo(maximum)> 0) {
-			maximum = thirdValue;
-		}
-		System.out.println("Maximum Float number is " + maximum);
-		return maximum;
-		
+	if(thirdValue.compareTo(maximum)> 0) {
+		maximum = thirdValue;
 	}
-	/**
-	 * Creating method findMaximum which return String
-	 * @param firstValue
-	 * @param secondValue
-	 * @param thirdValue
-	 * @return
-	 */
-	public String findMaximum(String firstValue, String secondValue, String thirdValue) {
-		String maximum = firstValue;
-		if (secondValue.compareTo(maximum) > 0) {
-            maximum = secondValue;
-        }
-        if (thirdValue.compareTo(maximum) > 0) {
-            maximum = thirdValue;
-        }
-        System.out.println("Maximum String number is " + maximum);
-        return maximum;
-    }		
+	System.out.println("Maximum Integer number is " + maximum);
+	return maximum;
+	}
 }
