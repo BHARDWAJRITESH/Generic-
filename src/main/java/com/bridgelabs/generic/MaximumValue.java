d5f973175b4e7bb1a3af21ecbaff78730196748b
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-10-11 19:47:38 +0530

    Add: Write and testing  to find maximum number from set of three integer

--- src/main/java/com/bridgelabs/generic/MaximumValue.java
+++ src/main/java/com/bridgelabs/generic/MaximumValue.java
@@ -1,7 +1,22 @@
 package com.bridgelabs.generic;
 
 public class  MaximumValue {
-	public static void main(String args[]) {
-		System.out.println(" Welcome to the Generic Program ");
+	/**
+	 * Creating method findMaximum
+	 * @param firstValue
+	 * @param secondValue
+	 * @param thirdValue
+	 * @return
+	 */
+	public Integer findMaximum(Integer firstValue, Integer secondValue, Integer thirdValue) {
+		Integer maximum = firstValue;
+		if(secondValue.compareTo(maximum)> 0) {
+			maximum = secondValue;
+		}
+		if(thirdValue.compareTo(maximum)> 0) {
+			maximum = thirdValue;
+		}
+		System.out.println("Maximum Integer number is " + maximum);
+		return maximum;		
 	}
 }
---
+++ src/test/java/com/bridgelabs/generic/MaximumValueTest.java
@@ -0,0 +1,28 @@
+package com.bridgelabs.generic;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MaximumValueTest {
+	MaximumValue maximum = new MaximumValue();
+	
+	@Test
+	public void shouldReturnMaximumAtFirstSpot() {
+		int value = maximum.findMaximum(70, 56, 30);
+		Assert.assertEquals(70, value);
+	}
+	
+	@Test
+	public void shouldReturnMaximumAtSecondSpot() {
+		int value = maximum.findMaximum(56, 70, 30);
+		Assert.assertEquals(70, value);
+	}
+	
+	@Test
+	public void shouldReturnMaximumAtThirdSpot() {
+		int value = maximum.findMaximum(30, 56, 70);
+		Assert.assertEquals(25, value);
+	}
+
+}
